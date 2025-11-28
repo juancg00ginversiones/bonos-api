5f9698656dc30d5f4aba76eb483b72dd9fddb862
juancg00ginversiones <juancg00ginversiones@gmail.com>
2025-11-28 13:21:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from calculadora import calcular_todo, curva_AL, curva_GD
 
 app = FastAPI()
+
+# ============================
+# HABILITAR CORS
+# ============================
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # permite llamadas desde cualquier página web
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
